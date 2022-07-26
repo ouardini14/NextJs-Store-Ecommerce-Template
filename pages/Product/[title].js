b7d9dc8825158326b6f9fb842cd4d2c3905e6e43
ouardini <ouardini14@gmail.com>
2022-07-26 12:08:56 +0100

    Template

---
+++ pages/Product/[title].js
@@ -0,0 +1,26 @@
+import React,{useState,useEffect} from 'react'
+import preview2 from '../../data/Preview2'
+import ProductPage from '../../components/ProductPage'
+
+export default function title({product}) {
+
+    if(!product){  return ( <div className='min-h-screen flex items-center justify-center text-5xl'>Not Found</div>)}
+  return (
+    <div
+    >
+        <ProductPage product={product} />
+    </div>
+  );
+}
+
+
+export async function getServerSideProps(context){
+    const productTitle=context.query.title?context.query.title:null
+    const  productIndex= preview2.findIndex(x => x.title.trim() ===productTitle.trim() )
+
+    return{
+      props:{
+        product:productIndex==-1?null :  preview2[productIndex]
+    }
+    }
+  }
